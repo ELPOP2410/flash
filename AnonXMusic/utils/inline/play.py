a6a3a06703683c815ca66d76da7e8fdb1dbbb641
ELPOP2410 <132166545+ELPOP2410@users.noreply.github.com>
2023-12-23 08:51:18 +0200

    Add files via upload

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -67,14 +67,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
        ],[
         InlineKeyboardButton(
-                text=_["• ᗩძძ TᏂᥱ ᙖ᥆T T᥆ Y᥆ᥙɾ ᘜɾ᥆ᥙρ •"], url=f"https://t.me/{app.username}?startgroup=true"
-        ],
-        [
-        InlineKeyboardButton(
-                text= "• S᥆υᖇᥴᥱ ᖴᥣᥲ᥉Ꮒ •", url=f"https://t.me/FLS_44"),
-        ],
-        [
-        InlineKeyboardButton(text="• ᥴᥣ᥆᥉ᥱ •", callback_data="close"),
+                text= "• S᥆υᖇᥴᥱ ᖴᥣᥲ᥉Ꮒ •", url=f"https://t.me/FLS_44"), 
         ],
     ]
     return buttons
@@ -90,16 +83,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],[
         InlineKeyboardButton(
-                text=_["• ᗩძძ TᏂᥱ ᙖ᥆T T᥆ Y᥆ᥙɾ ᘜɾ᥆ᥙρ •"], url=f"https://t.me/{app.username}?startgroup=true"
-        ],
-        [
-        InlineKeyboardButton(
-                text= "• S᥆υᖇᥴᥱ ᖴᥣᥲ᥉Ꮒ •", url=f"https://t.me/FLS_44"),
-        ],
-        [
-        InlineKeyboardButton(
-                text=_["• ᥴᥣ᥆᥉ᥱ •"],
-                callback_data="close",
+                text= "• S᥆υᖇᥴᥱ ᖴᥣᥲ᥉Ꮒ •", url=f"https://t.me/FLS_44"), 
         ],
     ]
     return buttons
