ccd98e3ae3f170fe0aaa8dbfd9023c4e287abff9
ELPOP2410 <132166545+ELPOP2410@users.noreply.github.com>
2023-12-26 22:56:29 +0200

    Add files via upload

--- AnonXMusic/plugins/tools/active.py
+++ AnonXMusic/plugins/tools/active.py
@@ -12,9 +12,9 @@ from AnonXMusic.utils.database import (
 )
 
 
-@app.on_message(filters.command(["activevc", "activevoice","المكالمات النشطه"],"") & SUDOERS)
+@app.on_message(filters.command(["activevc", "activevoice","المكلمات النشطه"],"") & SUDOERS)
 async def activevc(_, message: Message):
-    mystic = await message.reply_text("❃ انتظر لحظه من فضلك يتم الكشف الان ...")
+    mystic = await message.reply_text("» انتظر لحظه من فضلك يتم الكشف الان ...")
     served_chats = await get_active_chats()
     text = ""
     j = 0
@@ -36,17 +36,17 @@ async def activevc(_, message: Message):
         except:
             continue
     if not text:
-        await mystic.edit_text(f"❃ لا يوجد محادثات صوتيه نشطه الان {app.mention}.")
+        await mystic.edit_text(f"» لا يوجد محدثات صوتيه نشطه الان {app.mention}.")
     else:
         await mystic.edit_text(
-            f"<b>❃ المحادثات الصوتيه النشطه 👇🏻 :</b>\n\n{text}",
+            f"<b>» المحدثات الصوتيه النشطه 👇🏻 :</b>\n\n{text}",
             disable_web_page_preview=True,
         )
 
 
-@app.on_message(filters.command(["activev", "activevideo","محادثات الفيديو"],"") & SUDOERS)
+@app.on_message(filters.command(["activev", "activevideo","محدثات الفديديو"],"") & SUDOERS)
 async def activevi_(_, message: Message):
-    mystic = await message.reply_text("❃ انتظر لحظه يتم الكشف عن وجود محادثات فيديو ...")
+    mystic = await message.reply_text("» انتظر لحظه يتم الكشف عن وجود محدثات فيديو ...")
     served_chats = await get_active_video_chats()
     text = ""
     j = 0
@@ -68,9 +68,9 @@ async def activevi_(_, message: Message):
         except:
             continue
     if not text:
-        await mystic.edit_text(f"❃ لا يوجد كول فيديو نشط الان {app.mention}.")
+        await mystic.edit_text(f"» لا يوجد كول فيديو نشط الان {app.mention}.")
     else:
         await mystic.edit_text(
-            f"<b>❃ المحادثات فيديو النشطه الان :</b>\n\n{text}",
+            f"<b>» المحدثات فيديو النشطه الان :</b>\n\n{text}",
             disable_web_page_preview=True,
         )
