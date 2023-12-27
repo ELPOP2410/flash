543d1756fed0d1f59611238d3c911a48334a240b
ELPOP2410 <132166545+ELPOP2410@users.noreply.github.com>
2023-12-27 02:13:23 +0200

    Add files via upload

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -47,7 +47,7 @@ class Userbot(Client):
         )
 
     async def start(self):
-        LOGGER(__name__).info(f"تم تشغيل المساعدين...")
+        LOGGER(__name__).info(f"تم تشغيل المساعد...")
         if config.STRING1:
             await self.one.start()
             try:
