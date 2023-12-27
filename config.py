1ff11a162dbd6c88f9570cf8a19d946916453ac3
ELPOP2410 <132166545+ELPOP2410@users.noreply.github.com>
2023-12-27 04:18:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ELPOP2410/flash",
+    "https://github.com/lavatarl/avatar",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
